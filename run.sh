81ea47b59a0f26f6e96872a51a57893bb5bddb6f
Arghadeep <arghadeep.mazumder@teraki.com>
2023-09-06 12:46:39 +0200

    ip changed

--- run.sh
+++ run.sh
@@ -3,9 +3,9 @@
 HERE=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 echo "HERE=${HERE}"
 
-# DEVICE_IP="192.168.0.25"
-# export ROS_MASTER_URI="http://${DEVICE_IP}:11311"
-# export ROS_IP=${DEVICE_IP}
+DEVICE_IP="10.75.68.6"
+export ROS_MASTER_URI="http://${DEVICE_IP}:11311"
+
 
 source ${HERE}/../../devel/setup.bash
 roslaunch ros_bare_bones_sub ros_bare_bones_sub.launch
